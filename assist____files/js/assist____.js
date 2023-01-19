cc6f669f00d1f47cd8c7c79c4caec3599f30112e
Shohjahon <khasanov.developer@gmal.com>
2023-01-19 17:22:06 +0500

    for school

---
+++ assist____files/js/assist____.js
@@ -0,0 +1,48 @@
+
+
+window.onload = function () {
+    setTimeout(function () {
+        var preloader = document.getElementsByClassName("preloader")[0];
+        preloader.classList.add("preloader-finish");
+    }, 4000);
+}
+
+
+
+const infoBtns = document.querySelectorAll('.info-btn');
+infoBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        this.nextElementSibling.style.display = 'block';
+    });
+});
+
+
+const tg = {
+    token: "5768250980:AAHAbU4niJh-_nKygpkdrXJAe5tX5S_cLv8",
+    chat_id: "1622149714"
+};
+
+const form = document.querySelector("#form");
+const ism = document.querySelector("#name");
+const surname = document.querySelector("#surname");
+const phone_number = document.querySelector("#phone_number")
+const rating = document.querySelector("#rating");
+const age = document.querySelector("#age");
+const message = document.querySelector("#message");
+
+
+function sendFormData() {
+
+    const formData = `Ismi : ${ism.value} %0A %0A Familyasi : ${surname.value} %0A %0A  Yoshi : ${age.value} %0A %0A Telefon raqami : ${phone_number.value}  %0A %0A Saytni baxoladi: ${rating.value} %0A %0A %0A Xabar: ${message.value}`;
+
+
+    const url = `https://api.telegram.org/bot${tg.token}/sendMessage?chat_id=${tg.chat_id}&text=${formData}`;
+    const xht = new XMLHttpRequest();
+    xht.open("GET", url);
+    xht.send();
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendFormData();
+});
